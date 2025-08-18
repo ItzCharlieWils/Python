e06001924ffd8f7f430804c9b75863f0d2b0bda3
Charlie <106526858+ItzCharlieWils@users.noreply.github.com>
2025-08-18 15:55:53 +0700

    Add files via upload
    My day 1 learning python

---
+++ variables.py
@@ -0,0 +1,36 @@
+#String
+first_name = "Charlie"
+food = "Apple"
+email = "yourbro@bromail.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your Email-{email}")
+
+#Integer
+age = 20
+quantity = 10
+
+print(f"You are {age} years old")
+print(f"You want to buy {quantity} apples")
+
+#Float
+prize = 25.23
+gpa = 3.4
+
+print(f"it costs ${prize}")
+print(f"Your gpa is {gpa}")
+
+#Boolean
+is_student = True
+
+if is_student:
+    print("You are a Student.")
+else:
+    print("You are not a Student.")
+
+is_forsale = False
+if is_forsale:
+    print("That item is for Sale.")
+else:
+    print("That item is not for Sale.")
