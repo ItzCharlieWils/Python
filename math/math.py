40ebee9480223ace1da0398039743c93833ac484
Charlie <106526858+ItzCharlieWils@users.noreply.github.com>
2025-08-20 16:08:42 +0700

    Update math.py

--- math/math.py
+++ math/math.py
@@ -1,11 +1,17 @@
 import math
 
 x = 9.8
-
+print(f"original value of x = {x}")
 result = math.sqrt(x)
-print(result)
+print(f"math.sqrt(x) result = {result}")
 result = math.ceil(x)
-print(result)
+print(f"math.ceil(x) result = {result}")
 result = math.floor(x)
-print(result)
-print(math.pi)
+print(f"math.floor(x) result = {result}")
+result = math.sin(x)
+print(f"math.sin(x) result = {result}")
+result = math.cos(x)
+print(f"math.cos(x) result = {result}")
+result = math.tan(x)
+print(f"math.tan(x) result = {result}")
+print(f"math.pi result = {math.pi}")
