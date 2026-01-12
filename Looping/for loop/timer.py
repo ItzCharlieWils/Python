fc1edd24e674fd48397d46851b984db6d7a87fa9
Charlie <106526858+ItzCharlieWils@users.noreply.github.com>
2026-01-12 21:55:57 +0100

    Add files via upload

---
+++ Looping/for loop/timer.py
@@ -0,0 +1,10 @@
+import time
+
+my_time = int(input("Enter time in seconds: "))
+
+for x in reversed(range(my_time,0, 1)):
+    print(x)
+    time.sleep(1)
+time.sleep(my_time)
+
+print("Time's up")
