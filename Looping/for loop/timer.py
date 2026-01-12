01127461812fcf6153d9bcb31f1801b613c6c461
Charlie <106526858+ItzCharlieWils@users.noreply.github.com>
2026-01-12 21:56:40 +0100

    Change countdown step from 1 to 3 seconds

--- Looping/for loop/timer.py
+++ Looping/for loop/timer.py
@@ -2,9 +2,10 @@ import time
 
 my_time = int(input("Enter time in seconds: "))
 
-for x in reversed(range(my_time,0, 1)):
+for x in reversed(range(my_time,0, 3)):
     print(x)
     time.sleep(1)
 time.sleep(my_time)
+
 
 print("Time's up")
