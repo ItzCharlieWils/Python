28b00cda436cfe275c512e60f531a4f024d9422e
Charlie <106526858+ItzCharlieWils@users.noreply.github.com>
2026-02-04 12:13:46 +0100

    Fix formatting and comments in exercise1.py

--- function/exercise/exercise1.py
+++ function/exercise/exercise1.py
@@ -1,7 +1,9 @@
 #Create a simple function.
 #Instructions:
-#Create a function called show_message, the function must print the message:"This is my first function"
+#Create a function called show_message,
+#the function must print the message:"This is my first function"
 #Call the function 2 times.
 def show_message():
     print("This is my first function.")
 show_message()
+
