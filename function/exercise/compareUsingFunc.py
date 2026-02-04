408aac42889dd406e62f23a61f18315d0b29b956
Charlie <106526858+ItzCharlieWils@users.noreply.github.com>
2026-02-04 11:01:46 +0100

    Add files via upload

---
+++ function/exercise/compareUsingFunc.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+def compare(a, b):
+    if a > b:
+        return f"{a} is greater than {b}"
+    elif a < b:
+        return f"{a} is less than {b}"
+    elif a == b:
+         return f"{a} is equal to {b}"
+    else:
+        return f"{a} is equal to {b}"
+result = compare(a, b)
+print(result)
