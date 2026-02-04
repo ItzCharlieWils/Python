f8d0e876799753924e9cbd64afb31084d96bd5f0
Charlie <106526858+ItzCharlieWils@users.noreply.github.com>
2026-02-04 10:20:30 +0100

    Add files via upload

---
+++ Looping/Exercise/function/exercise/ex1.py
@@ -0,0 +1,7 @@
+# This is a function that prints a greeting message
+# Function starts with the keyword 'def' followed by the function name and parentheses
+# The function contains a print statement or other code to execute
+def myfunc():
+    print("Hello, How are you?")
+
+myfunc()
